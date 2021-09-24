466c5c7fba074036de65b496b15fcc6ae2ec3d56
Myokatoshi <sylvain.motta@gmail.com>
2021-09-24 17:23:02 +0200

    test js avec l'api

--- front-end/js/products.js
+++ front-end/js/products.js
@@ -7,8 +7,24 @@
 })()
 
 function getarticles() {
-    return fectch()
+    return fectch("https://localhost:3000/api/teddies")
     .then(function(httpbodyResponse) {
         return httpbodyResponse.json()
     })
+    .then(function(articles) {
+        return articles
+    })
+    .catch(function(error) {
+        alert(error)
+    })
+}
+
+function displayArticle(article) {
+    const templateElt = document.getElementById("templateArticle")
+    const cloneElt = document.importNode(templateElt.content, true)
+
+    cloneElt.getElementById("blog__name").textContent = article.name
+    cloneElt.getElementById("blog__price").textContent = article.price
+
+    document.getElementById("main").appendChild(cloneElt)
 }
