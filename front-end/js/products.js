56858dc4f3dbea16c83216afb647997e9fde4b77
Myokatoshi <sylvain.motta@gmail.com>
2021-09-29 11:27:49 +0200

    creation d'une page pour tester l'API

--- front-end/js/products.js
+++ front-end/js/products.js
@@ -2,13 +2,28 @@
     const articles = await getArticles()
 
     for (article of articles) {
-        displayArticle(article)
+        displayArticles(article)
     }
 })()
 
-function getarticles() {
-    return fectch("http://localhost:3000/api/teddies")
+const buttonElement = document.getElementById('btn-oriteddies');
+
+
+buttonElement.addEventListener('click', function (event) {
+    var articles = document.getElementsByClassName("article");
+    for (var i =0; i < articles.length; i+=1) {
+        articles[i].style.display = "flex";
+    } 
+    document.getElementById("teddy_imageUrl").src = new URL("http://localhost:3000/images/teddy_1.jpg");
+    document.getElementById("teddy_name").innerHTML = "Norbert";
+    document.getElementById("teddy_description").innerHTML = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+    document.getElementById("teddy_price").innerHTML = "2900";  
+});
+
+function getArticles() {
+    return fetch("http://localhost:3000/api/teddies")
     .then(function(httpbodyResponse) {
+        console.log(httpbodyResponse)
         return httpbodyResponse.json()
     })
     .then(function(articles) {
@@ -19,12 +34,13 @@ function getarticles() {
     })
 }
 
-function displayArticle(article) {
-    const templateElt = document.getElementById("templateArticle")
+function displayArticles(article) {
+    const templateElt = document.getElementById("teddy_article")
     const cloneElt = document.importNode(templateElt.content, true)
 
-    cloneElt.getElementById("blog__name").textContent = article.name
-    cloneElt.getElementById("blog__price").textContent = article.price
+    cloneElt.getElementById("teddy_name").textContent = article.name
+    cloneElt.getElementById("teddy_description").textContent = article.description
+    cloneElt.getElementById("teddy_price").textContent = article.price
 
-    document.getElementById("main").appendChild(cloneElt)
+    document.getElementById("section_article").appendChild(cloneElt)
 }
